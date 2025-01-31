23fc5cd5d3746e1357f552a3f657b2081aa012da
tuyen <tuyennh@nhanhoagroup.vn>
2025-02-01 04:13:07 +0700

    .

--- Advanced Redux/src/App.js
+++ Advanced Redux/src/App.js
@@ -3,8 +3,8 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 
 let isInitial = true;
 function App() {
@@ -14,46 +14,17 @@ function App() {
    const notification = useSelector((state) => state.ui.notification);
 
    useEffect(() => {
-      const sendCartData = async () => {
-         dispatch(
-            uiActions.showNotification({
-               status: "pending",
-               title: "Sending...",
-               message: "Sending cart data!",
-            })
-         );
-         const response = await fetch(
-            "https://react-617cc-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-            { method: "PUT", body: JSON.stringify(cart) }
-         );
+      dispatch(fetchCartData());
+   }, [dispatch]);
 
-         if (!response.ok) {
-            throw new Error("Sending cart data failed");
-         }
-
-         // const responseData = await response.json();
-
-         dispatch(
-            uiActions.showNotification({
-               status: "success",
-               title: "Success!",
-               message: "Send cart data successfully!",
-            })
-         );
-      };
+   useEffect(() => {
       if (isInitial) {
          isInitial = false;
          return;
       }
-      sendCartData().catch((error) => {
-         dispatch(
-            uiActions.showNotification({
-               status: "error",
-               title: "Error!",
-               message: "Sending cart data failed!",
-            })
-         );
-      });
+      if (cart.changed) {
+         dispatch(sendCartData(cart));
+      }
    }, [cart, dispatch]);
    return (
       <>
