eba5a6f8a8108d13b623083cf39fe4faf55dd98e
tuyennh <hoangtuyen1106@gmail.com>
2025-02-11 01:43:25 +0700

    .

--- 27 Patterns/src/App.jsx
+++ 27 Patterns/src/App.jsx
@@ -7,86 +7,87 @@ import amazonImg from "./assets/amazon-river.jpg";
 import caribbeanImg from "./assets/caribbean-beach.jpg";
 import desertImg from "./assets/desert-dunes.jpg";
 import forestImg from "./assets/forest-waterfall.jpg";
+import Place from "./Place.jsx";
 
 const PLACES = [
-    {
-        id: "african-savanna",
-        image: savannaImg,
-        title: "African Savanna",
-        description: "Experience the beauty of nature.",
-    },
-    {
-        id: "amazon-river",
-        image: amazonImg,
-        title: "Amazon River",
-        description: "Get to know the largest river in the world.",
-    },
-    {
-        id: "caribbean-beach",
-        image: caribbeanImg,
-        title: "Caribbean Beach",
-        description: "Enjoy the sun and the beach.",
-    },
-    {
-        id: "desert-dunes",
-        image: desertImg,
-        title: "Desert Dunes",
-        description: "Discover the desert life.",
-    },
-    {
-        id: "forest-waterfall",
-        image: forestImg,
-        title: "Forest Waterfall",
-        description: "Listen to the sound of the water.",
-    },
+  {
+    id: "african-savanna",
+    image: savannaImg,
+    title: "African Savanna",
+    description: "Experience the beauty of nature.",
+  },
+  {
+    id: "amazon-river",
+    image: amazonImg,
+    title: "Amazon River",
+    description: "Get to know the largest river in the world.",
+  },
+  {
+    id: "caribbean-beach",
+    image: caribbeanImg,
+    title: "Caribbean Beach",
+    description: "Enjoy the sun and the beach.",
+  },
+  {
+    id: "desert-dunes",
+    image: desertImg,
+    title: "Desert Dunes",
+    description: "Discover the desert life.",
+  },
+  {
+    id: "forest-waterfall",
+    image: forestImg,
+    title: "Forest Waterfall",
+    description: "Listen to the sound of the water.",
+  },
 ];
 function App() {
-    return (
-        <main>
-            <section>
-                <h2>Why work with us?</h2>
-                <Accordion className="accordion">
-                    <Accordion.Item id="experience" className="accordion-item">
-                        <Accordion.Title className="accordion-item-title">
-                            We got 20 years of experience222222222222222222
-                        </Accordion.Title>
-                        <Accordion.Content className="accordion-item-content">
-                            <article>
-                                <p>You can't go wrong with us.</p>
-                                <p>
-                                    We are in the business of planning highly
-                                    indivialized vacation trips for more than 20
-                                    years.
-                                </p>
-                            </article>
-                        </Accordion.Content>
-                    </Accordion.Item>
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We got 20 years of experience222222222222222222
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>You can't go wrong with us.</p>
+                <p>
+                  We are in the business of planning highly indivialized
+                  vacation trips for more than 20 years.
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
 
-                    <Accordion.Item
-                        id="local-guides"
-                        className="accordion-item"
-                    >
-                        <Accordion.Title className="accordion-item-title">
-                            We are worling with local guides
-                        </Accordion.Title>
-                        <Accordion.Content className="accordion-item-content">
-                            <article>
-                                <p>You can't go wrong with us.</p>
-                                <p>
-                                    Instead, we are working with local guides to
-                                    ensure a safe and pleasant vacation.
-                                </p>
-                            </article>
-                        </Accordion.Content>
-                    </Accordion.Item>
-                </Accordion>
-            </section>
-            <section>
-                <SearchableList items={PLACES} />
-                <SearchableList items={["item 1", "item 2"]} />
-            </section>
-        </main>
-    );
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We are worling with local guides
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>You can't go wrong with us.</p>
+                <p>
+                  Instead, we are working with local guides to ensure a safe and
+                  pleasant vacation.
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+      <section>
+        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList items={["item 1", "item 2"]} itemKeyFn={(item) => item}>
+          {(item) => item}
+        </SearchableList>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 27 Patterns/src/components/SearchableList/SearchableList.jsx
+++ 27 Patterns/src/components/SearchableList/SearchableList.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-export default function SearchableList({ items, children }) {
-    const [searchTerm, setSearchTerm] = useState("");
+export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef();
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const searchResults = items.filter((item) =>
-        JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-    const handleChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
-    return (
-        <div className="searchable-list">
-            <input type="search" placeholder="Search" onChange={handleChange} />
-            <ul>
-                {searchResults.map((item, index) => (
-                    <li key={index}>{children}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  const handleChange = (event) => {
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchTerm(event.target.value);
+    }, 500);
+  };
+  return (
+    <div className="searchable-list">
+      <input type="search" placeholder="Search" onChange={handleChange} />
+      <ul>
+        {searchResults.map((item, index) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
